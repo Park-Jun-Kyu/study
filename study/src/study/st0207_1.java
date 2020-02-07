2f30b83e190116dac2d71393868e1f31683fa570
Park-Jun-Kyu <dailykq@naver.com>
2020-02-07 21:07:25 +0900

    0207 stydy
    학점

---
+++ study/src/study/st0207_1.java
@@ -0,0 +1,53 @@
+package study;
+
+import java.util.Scanner;
+
+public class st0207_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 성적을 입력하여 학점을 계산하는 프로그램을 만드세요
+		// 90점 이상 A(95점 이상 A+)
+		// 80점 이상 B(85점 이상 B+) 
+		// 80점 미만 F
+		// input ) 성적을 입력하세요 : 90
+		// output )  당신의 학점은 A- 입니다.
+		
+		String grade = "";//학점
+		
+		
+		
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("성적을 입력하세요 ");
+		int num = scan.nextInt();
+		
+		
+		
+		if ( num >= 90 ) {
+			grade = "A";
+			if ( num >= 95) {
+				grade = grade + "+";
+			}
+				
+			}else if(num >= 80) {
+				grade ="B";
+			  if(num >= 85) 
+				grade = grade + "+";
+			}else { 
+				grade = "F";
+			}
+			
+			
+		
+		
+		System.out.println("당신의 학점은" + grade);
+		
+		
+		
+		
+		
+	} //main
+
+}//class
